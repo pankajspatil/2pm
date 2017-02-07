3e7a4af1a2fc13d18ae73cd1b4c955cb3b1926ba
pankaj <Pankaj@Patil-PC>
2017-02-07 20:23:27 +0530

    Added new user

---
+++ WebApp/resources/sql/2pm_v4.sql
@@ -0,0 +1,3 @@
+UPDATE `two_pm`.`user_master` SET `first_name`='2pm', `last_name`='2pm', `email`='twopm@gmail.com', `user_name`='2pm', `password`='3priminister' WHERE `user_id`='2';
+UPDATE `two_pm`.`user_master` SET `role_id`='1' WHERE `user_id`='2';
+UPDATE `two_pm`.`user_menu_master` SET `menu_description`='Report' WHERE `menu_id`='6';
